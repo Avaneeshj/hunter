bbc1180bf6793f3ad3d2c5381cf6612bed1b61bf
Avaneeshj <52447478+Avaneeshj@users.noreply.github.com>
2019-07-11 18:32:34 +0530

    Update 07.py

--- 07.py
+++ 07.py
@@ -1,4 +1,4 @@
-a=int(input())
+aj=int(input())
 b=list(map(int,input().split()))
 l=[]
 m=[]
@@ -11,7 +11,7 @@ while(c<100000):
 while(d<100000):
     m.append(d)
     d+=2
-for i in range(a):
+for i in range(aj):
     if((b[i] in l) and (i in m)):
         n.append(b[i])
     elif((b[i] in m) and (i in l)):
