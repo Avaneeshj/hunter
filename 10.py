8bc1e215122b3d47629cb6a65b8e26285ef96147
Avaneeshj <52447478+Avaneeshj@users.noreply.github.com>
2019-07-13 13:11:57 +0530

    Add files via upload

---
+++ 10.py
@@ -0,0 +1,16 @@
+a,b=map(int,input().split())
+c=list(map(int,input().split()))
+d=list(map(int,input().split()))
+e=0
+if a>b:
+    for i in range(b):
+        if((d[i]) in c) and d.count(i)<=c.count(i):
+            e+=1
+        else:
+            e=0
+else:
+    print("NO")
+if(e==b):
+    print("YES")
+else:
+    print("NO")
