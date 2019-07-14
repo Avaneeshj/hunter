e07ae843e17e05ae109cb3da72d15fd85697d64f
Avaneeshj <52447478+Avaneeshj@users.noreply.github.com>
2019-07-14 15:04:06 +0530

    Add files via upload

---
+++ 34.py
@@ -0,0 +1,20 @@
+from itertools import permutations
+a=input()
+c=[]
+f=[]
+b=permutations(a)
+for i in list(b):
+    c.append(i)
+for j in range(0,len(c)):
+    d="".join(c[j])
+    e=int(d)
+    f.append(e)
+f.sort()
+for i in range(len(f)):
+    f[i]=int(f[i])-int(a)
+    if f[i]==0:
+        z=i
+if(f[z]==f[-1]):
+    print("impossible")
+else:
+    print(f[z+1]+int(a))
