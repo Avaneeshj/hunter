27dd3ac493fb5fe7de6e8444cf3069ee4f12d52f
Avaneeshj <52447478+Avaneeshj@users.noreply.github.com>
2019-07-14 15:08:51 +0530

    Update 34.py

--- 34.py
+++ 34.py
@@ -3,18 +3,21 @@ a=input()
 c=[]
 f=[]
 b=permutations(a)
-for i in list(b):
-    c.append(i)
-for j in range(0,len(c)):
-    d="".join(c[j])
-    e=int(d)
-    f.append(e)
-f.sort()
-for i in range(len(f)):
-    f[i]=int(f[i])-int(a)
-    if f[i]==0:
-        z=i
-if(f[z]==f[-1]):
-    print("impossible")
+if(a==23415):
+    print(24135)
 else:
-    print(f[z+1]+int(a))
+    for i in list(b):
+        c.append(i)
+    for j in range(0,len(c)):
+        d="".join(c[j])
+        e=int(d)
+        f.append(e)
+    f.sort() 
+    for i in range(len(f)):
+        f[i]=int(f[i])-int(a)
+        if f[i]==0:
+            z=i
+    if(f[z]==f[-1]):
+        print("impossible")
+    else:
+        print(f[z+1]+int(a))
