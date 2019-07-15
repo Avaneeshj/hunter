93340c76d4de90821b0048746a3945b1d7d55327
Avaneeshj <52447478+Avaneeshj@users.noreply.github.com>
2019-07-15 12:01:05 +0530

    Add files via upload

---
+++ 40.py
@@ -0,0 +1,19 @@
+a=int(input())
+g=len(str(a))
+c=0
+while(a>0):
+    b=a%10
+    c+=b
+    a=int(a/10)
+h=len(str(c))
+d=[]
+f=0
+for i in range(h):
+    e=c%10
+    d.append(e)
+    print(d)
+    c=int(c/10)
+if(d[:]==d[::-1]):
+    print("YES")
+else:
+    print("NO")
