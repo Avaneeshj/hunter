5a8293673a9ed2050962b243f67a925e8c65e35b
Avaneeshj <52447478+Avaneeshj@users.noreply.github.com>
2019-07-19 15:52:41 +0530

    Add files via upload

---
+++ 79.py
@@ -0,0 +1,17 @@
+a=int(input())
+b=list(map(int,input().split()))
+c=b
+while(len(c)>0):
+    d=len(c)
+    if(d%2!=0):
+        e=int(d//2)
+        print(c[e])
+        c.remove(c[e])
+        print(c)
+    elif(d%2==0):
+        f=int(d//2)
+        g=f-1
+        h=int((c[f]+c[g])/2)
+        print(h)
+        c.remove(c[f])
+        c.remove(c[g])
